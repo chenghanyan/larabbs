4b67931dbec70b7ddbaeb85ae5f214b88c8c6445
chenghanyan <549176227@qq.com>
2018-04-16 22:45:27 +0800

    Finsh base page layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,9 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<meta name="csrf-token" content="{{csrf_toen()}}">
-	<title>@yield('title',Larabbs) -实战教程</title>
-	<style type="text/css">
-		<link href="{{asset('css/app.css')}}" rel="stylesheet"/ >
-	</style>
+	<meta name="csrf-token" content="{{csrf_token()}}">
+	<title>@yield('title') -实战教程</title>
+	<link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
 </head>
 <body>
 	<div id="app" class="{{route_class()}}-page">
