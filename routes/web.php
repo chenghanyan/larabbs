6b7c32ea01005b004799ba6f5fcf6f38a56ef289
chenghanyan <549176227@qq.com>
2018-04-17 15:08:09 +0800

    User profile page

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,6 @@ Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm'
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+//resource Routes
+Route::resource('users' , 'UserController' ,['only' => ['show','update','edit']]);
