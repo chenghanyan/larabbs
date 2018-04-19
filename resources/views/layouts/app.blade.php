1e2740df5c6dd540caa37fe88b0b2875dfa52602
chenghanyan <549176227@qq.com>
2018-04-19 10:10:02 +0800

    WYSIWYG Editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
 	<meta name="csrf-token" content="{{csrf_token()}}">
 	<title>@yield('title') -实战教程</title>
 	<link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+	  @yield('styles')
 </head>
 <body>
 	<div id="app" class="{{route_class()}}-page">
@@ -19,5 +20,6 @@
 		@include('layouts._footer')
 	</div>
 	<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+	 @yield('scripts')
 </body>
 </html>
