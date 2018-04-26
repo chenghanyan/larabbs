fc72f693238a66ca63b93ab9cd62ab442228a4fa
vagrant <vagrant@homestead>
2018-04-26 08:25:07 +0000

    administrator install

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,26 @@ class User extends Authenticatable
     use Notifiable {
         notify as protected laravelNotify;
     }
+
+    //Eloquent修改器 修改密码属性
+    public function setPasswordAttribute($value)
+    {
+        //如果值的长度等于60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+            $value = bcrypt($value); //未加密在此加密
+        }
+        $this->attributes['password'] = $value;
+    }
+    public function setAvatarAttribute($path)
+    {
+        // 如果不是'http',字符串开头，那就是从后台上传的，需要补全ur
+         if (!starts_with($path, 'http')) {
+            //拼接完整的URL
+            $path = config('app.url') . "/uploads/images/avatars/$path";
+
+            $this->attributes['avatar'] = $path;
+         }
+    }
     public function notify($instance)
     {
         // 如果要通知的人是当前用户，就不必通知了！
