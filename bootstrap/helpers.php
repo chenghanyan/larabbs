e774eb4e4293093002009309e2b08ccfd0d2306f
vagrant <vagrant@homestead>
2018-04-26 09:31:09 +0000

    e3333

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -17,3 +17,28 @@ function make_excerpt($value, $length=200)
 	$excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
 	return str_limit($excerpt, $length);
 }
+function model_admin_link($title, $model)
+{
+	return model_link($title, $model, 'admin');
+}
+function model_link ($title, $model, $prefix='')
+{
+	//获取数据模型的复数蛇形命名
+	$model_name = model_plural_name($model);
+
+	//初始化前缀
+	$prefix = $prefix ? "/$prefix/" : '/';
+
+	//使用站点URL拼接全量 URL
+	$url = config('app.url') . $prefix . $model_name . '/' . $model->id;
+
+	//拼接HTML A标签，并返回
+	return '<a href="'. $url .'" target="_blank">' . $title . '</a>';
+}
+function model_plural_name($model)
+{
+	//从实体中获取完整类名，例如：App\Models\User
+	$full_class_name = get_class($model);
+
+
+}
