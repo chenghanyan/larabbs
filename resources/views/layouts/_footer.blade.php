3de6bcb48ea184762965500f26c1ca8f9ecbd953
vagrant <vagrant@homestead>
2018-04-27 02:09:18 +0000

    Admin can manage site setting

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -5,7 +5,7 @@
 			<span style="color:#e27575;font-size: 14px">❤</span>
 		</p>
 		<p class="pulll-right">
-			<a href="mailto:549176227@qq.com">联系我们</a>
+			<a href="mailto:{{ setting('contact_email') }}">联系我们</a>
 		</p>
 	</div>
 </footer>
