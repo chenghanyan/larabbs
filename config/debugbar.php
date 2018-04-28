881cdbef84e1ac0a7feac503c6469de67a28d650
vagrant <vagrant@homestead>
2018-04-28 08:04:12 +0000

    fix laravel-debugbar

--- config/debugbar.php
+++ config/debugbar.php
@@ -14,7 +14,8 @@ return [
      |
      */
 
-    'enabled' => env('APP_DEBUG', null),
+    // 'enabled' => env('APP_DEBUG', false),
+    'enabled' => env('DEBUGER_ENABLE', false),
     'except' => [
         //
     ],
